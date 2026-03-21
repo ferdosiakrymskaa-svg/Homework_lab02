77bb14d06541ea284f758427148ef59a82fedf15
ferdosiakrymskaa-svg <ferdosiakrymskaa@gmail.com>
2026-03-21 19:59:10 +0000

    Избавились от using namespace std

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <string>
-using namespace std;
+
 
 int main()
 {
-    string name;
-    cout << "Input your name: ";
-    cin >> name;
-    cout << "Hello world from "<<name << endl;
+    std::string name;
+    std::cout << "Input your name: ";
+    std::cin >> name;
+    std::cout << "Hello world from "<<name << std::endl;
     return 0;
 }
