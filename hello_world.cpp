45bc53ec951a22c4f5370f11897b32a8768d1fa7
Ilyasov_Ilya <ferdosiakrymskaa@gmail.com>
2026-03-22 01:22:20 +0300

    Update hello_world.cpp
    Добавил перевод на другой язык

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,7 +4,7 @@
 
 int main()
 {
-    std::string name;//Переменная, хранящая имя пользователя
+    std::string name;//Переменная, хранящая имя пользователя(value, which save user name)
     std::cout << "Input your name: ";//Печатает комментарий, который говорит, что пользователю необходимо ввести
     std::cin >> name;//Ввод имени пользователя
     std::cout << "Hello world from "<<name << std::endl;//Вывод программы
